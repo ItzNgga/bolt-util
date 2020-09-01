10466d295d7071804ba5dc6d3181dfecfdfe2b99
ItzNgga <rangganak094@gmail.com>
2020-09-02 00:50:33 +0700

    Fix bugs and add some features

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const clear = require('clear');
 const figlet = require('figlet');
 const nama = require('./lib/name.js')
 const unlock = require('./lib/unlock.js')
+const imei = require('./lib/imei.js')
 
 // ____     ___    _       _____     _   _   _____   ___   _
 // | __ )   / _ \  | |     |_   _|   | | | | |_   _| |_ _| | |
@@ -40,9 +41,7 @@ inquirer.prompt([
                   figlet.textSync('RANDOM IMEI', { horizontalLayout: 'full' })
                 )
             );
-            var imei = require('node-imei');
-            var IMEI= new imei();
-            console.log(`\nYour IMEI: ${IMEI.device('Samsung','GalaxyS3')}`)
+            console.log('\nYour IMEI: '+imei.gen())
             require('child_process').spawnSync("pause", {shell: true, stdio: [0, 1, 2]});
             break
     }
