a9aca43b0f3c6df414640a4681dcbfd8a888789e
ItzNgga <rangganak094@gmail.com>
2020-09-01 20:47:28 +0700

    Updating new contents

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
 const nama = require('./lib/name.js')
-
+const unlock = require('./lib/unlock.js')
 clear()
 console.log(
     chalk.green(
@@ -14,18 +14,28 @@ console.log('\n\nBOLT Modem utility')
 inquirer.prompt([
     {
         type: "list",
-        message: "Pilih aksi",
+        message: "Pilih utilitas\n",
         name: "tipe",
-        choices: ["ChangeName","Unlock"]
+        choices: ["ChangeName","Unlock","IMEI"]
     }
 ]).then(async (answer) => {
     switch (answer.tipe) {
         case 'ChangeName':
             nama.ChangeName();
             break;
-    
-        default:
-            console.log('Mohon masukan tipe utilitas')
-            process.exit(22);
+        case 'Unlock':
+            unlock.unlock();
+            break;
+        case 'IMEI':
+            clear()
+            console.log(
+                chalk.green(
+                  figlet.textSync('RANDOM IMEI', { horizontalLayout: 'full' })
+                )
+            );
+            var imei = require('node-imei');
+            var IMEI= new imei();
+            console.log(`\nYour IMEI: ${IMEI.device('Samsung','GalaxyS3')}`)
+            break
     }
 })
